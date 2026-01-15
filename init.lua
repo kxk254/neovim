d39be767240057ce9029ccc79bf26d4af16608c1
konno <konno.kenji@gmail.com>
2026-01-15 06:57:48 +0000

    test

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+
+-- Set leader key to space
+vim.g.mapleader = " "   -- global leader
+
 -- vim.opt.rtp:prepend("~/.local/share/nvim/lazy/lazy.nvim")
 vim.opt.rtp:prepend(vim.fn.stdpath("data") .. "/lazy/lazy.nvim")
 require("lazy").setup({
@@ -15,3 +19,21 @@ vim.api.nvim_create_autocmd("VimEnter",{
 -- Load core config
 require("options")
 require("keymaps")
+
+
+-- Enable built-in OSC 52 support (Neovim ≥ 0.10)
+-- If you're on older Neovim, this still usually works via the fallback:works
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
+
+-- Optional but very convenient: make normal yanks go to system clipboard
+vim.opt.clipboard = 'unnamedplus'
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,9 +1,18 @@
 vim.keymap.set("n", "tn", function()
-  if vim.o.relativenumber then
-    vim.o.relativenumber = false
-    vim.o.number = true
-  else
-    vim.o.relativenumber = true
-    vim.o.number = true
-  end
-end, { desc = "Toggle line numbers" })
+  vim.wo.relativenumber = not vim.wo.relativenumber
+  vim.wo.number = true -- always keep absolute numbers on
+end, { desc = "Toggle relative numbers" })
+
+-- Leader for Git shortcuts
+vim.keymap.set("n", "<leader>gs", ":Git<CR>", { desc = "Git status" })
+vim.keymap.set("n", "<leader>gc", ":Git commit<CR>", { desc = "Git commit" })
+vim.keymap.set("n", "<leader>ga", ":Git add %<CR>", { desc = "Git add current file" })
+vim.keymap.set("n", "<leader>gA", ":Git add .<CR>", { desc = "Git add all changes" })
+vim.keymap.set("n", "<leader>gp", ":Git push<CR>", { desc = "Git push" })
+vim.keymap.set("n", "<leader>gf", ":Git fetch<CR>", { desc = "Git fetch" })
+vim.keymap.set("n", "<leader>gl", ":Git log<CR>", { desc = "Git log" })
+vim.keymap.set("n", "<leader>gd", ":Git diff<CR>", { desc = "Git diff" })
+vim.keymap.set("n", "<leader>gD", ":Gvdiffsplit<CR>", { desc = "Git diff split" })
+vim.keymap.set("n", "<leader>gb", ":Git branch<CR>", { desc = "Git branches" })
+vim.keymap.set("n", "<leader>gco", ":Git checkout ", { desc = "Git checkout" })
+
